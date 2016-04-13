21949efa86cf67d79fdfc2a7c1de4747f802af00
Revill1 <adamredlinski@gmail.com>
2016-04-13 22:47:14 +0200

    Dodanie testów sprawdzajacych poprawnosc metody calculateJackardSimilarity z klasy SimilarityFinder, dodanie metody check do klasy search

--- src/main/java/edu/iis/mto/similarity/Search.java
+++ src/main/java/edu/iis/mto/similarity/Search.java
@@ -10,5 +10,14 @@ public class Search {
 	public static SequenceSearcher getSequenceSearcher() {
 		 		return new SequenceReturn();
 		 	}
+	
+		 	public static int check(Object object, Method method) {
+		 		try {
+		 			return object.getClass().getDeclaredField(method.getName()).getInt(object);
+		 		} catch (Exception e) {
+		 			e.printStackTrace();
+		 		}
+		 		return -1;
+		 	}
 
 }
